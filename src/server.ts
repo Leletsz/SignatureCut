9901c8fb604fcaad15dfd9019898451ea469de2e
devlets <leandrovictor2011@gmail.com>
2026-09-04 18:37:04 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
+import type { Request, Response } from "express";
 import express from "express";
-import cors from "cors";
 import { router } from "./routes.js";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.use(router);
 
-app.listen(3333, () => console.log("SERVER ONLINE :)"));
+app.listen(3333, () => console.log("SERVER ONLINE :) "));
